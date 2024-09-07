3089510e95e47c0d20ae7664fe9255a7ad551b23
Leamonz <3262305069@qq.com>
2024-09-07 10:07:20 +0800

    Complete Task0.1/2/3

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -3,28 +3,235 @@
 import math
 
 # ## Task 0.1
-from typing import Callable, Iterable
+from typing import Callable, Iterable, List
 
 #
 # Implementation of a prelude of elementary functions.
+# TODO: Implement for Task 0.1.
+
 
 # Mathematical functions:
 # - mul
+def mul(x: float, y: float) -> float:
+    """Computes the multiplication of x and y
+
+    Args:
+        x (float): x
+        y (float): y
+
+    Returns:
+        float: x * y
+    """
+    return x * y
+
+
 # - id
+def id(x: float) -> float:
+    """returns x identically
+
+    Args:
+        x (float): x
+
+    Returns:
+        float: x
+    """
+    return x
+
+
 # - add
+def add(x: float, y: float) -> float:
+    """Computes the summation of x and y
+
+    Args:
+        x (float): x
+        y (float): y
+
+    Returns:
+        float: x + y
+    """
+    return x + y
+
+
 # - neg
+def neg(x: float) -> float:
+    """Negates a number
+
+    Args:
+        x (float): x
+
+    Returns:
+        float: -x
+    """
+    return -x
+
+
 # - lt
+def lt(x: float, y: float) -> bool:
+    """Checks if x is less than y
+
+    Args:
+        x (float): x
+        y (float): y
+
+    Returns:
+        bool: x < y
+    """
+    return x < y
+
+
 # - eq
+def eq(x: float, y: float) -> bool:
+    """Checks if x is equal to y
+
+    Args:
+        x (float): x
+        y (float): y
+
+    Returns:
+        bool: x == y
+    """
+    return x == y
+
+
 # - max
+def max(x: float, y: float) -> float:
+    """Returns the maximum of x and y
+
+    Args:
+        x (float): x
+        y (float): y
+
+    Returns:
+        float: x if lt(y, x) else y
+    """
+    return x if lt(y, x) else y
+
+
 # - is_close
+def is_close(x: float, y: float) -> bool:
+    """Checks if x and y are close
+
+    Args:
+        x (float): x
+        y (float): y
+
+    Returns:
+        float: |x - y| < 1e-6
+    """
+    return abs(x - y) < 1e-6
+
+
 # - sigmoid
+def sigmoid(x: float) -> float:
+    """Calculates the sigmoid of x
+
+    Args:
+        x (float): x
+
+    Returns:
+        float: sigmoid(x) = 1 / (1 + exp(-x))
+    """
+    return 1.0 / (1.0 + math.exp(-x)) if x >= 0 else math.exp(x) / (1.0 + math.exp(x))
+
+
 # - relu
+def relu(x: float) -> float:
+    """Applies ReLU on x
+
+    Args:
+        x (float): x
+
+    Returns:
+        float: max(0, x)
+    """
+    return max(0, x)
+
+
 # - log
+def log(x: float) -> float:
+    """Calculates the natural logarithm of x
+
+    Args:
+        x (float): x
+
+    Returns:
+        float: ln(x)
+    """
+    assert x != 0
+    return math.log(x)
+
+
 # - exp
+def exp(x: float) -> float:
+    """Calculates the exponential function
+
+    Args:
+        x (float): x
+
+    Returns:
+        float: exp(x)
+    """
+    return math.exp(x)
+
+
 # - log_back
+def log_back(x: float, y: float) -> float:
+    """Computes the derivative of log(x) and times y. The derivative of ln(x) is 1 / x
+
+    Args:
+        x (float): x
+        y (float): y
+
+    Returns:
+        float: y / x
+    """
+    assert x != 0
+    return y / x
+
+
 # - inv
+def inv(x: float) -> float:
+    """Computes the reciprocal of x
+
+    Args:
+        x (float): x
+
+    Returns:
+        float: 1 / x
+    """
+    assert x != 0
+    return 1.0 / x
+
+
 # - inv_back
+def inv_back(x: float, y: float) -> float:
+    """Computes the derivative of 1 / x and times y. The derivative of 1 / x is -1 / x**2
+
+    Args:
+        x (float): x
+        y (float): y
+
+    Returns:
+        float: -y / x**2
+    """
+    assert x != 0
+    return -y / x**2
+
+
 # - relu_back
+def relu_back(x: float, y: float) -> float:
+    """Computes the derivative of relu(x) and times y. This acts like a gate controlled by x.
+
+    Args:
+        x (float): x
+        y (float): y
+
+    Returns:
+        float: y if x > 0 else 0
+    """
+    return y if x > 0 else 0
+
+
 #
 # For sigmoid calculate as:
 # $f(x) =  \frac{1.0}{(1.0 + e^{-x})}$ if x >=0 else $\frac{e^x}{(1.0 + e^{x})}$
@@ -32,23 +239,103 @@ from typing import Callable, Iterable
 # $f(x) = |x - y| < 1e-2$
 
 
-# TODO: Implement for Task 0.1.
-
-
 # ## Task 0.3
 
 # Small practice library of elementary higher-order functions.
 
+# TODO: Implement for Task 0.3.
+
+
 # Implement the following core functions
 # - map
+def map(fn: Callable[[float], float]):
+    # Closure
+    def process(ls: Iterable[float]):
+        return [fn(e) for e in ls]
+
+    return process
+
+
 # - zipWith
+def zipWith(
+    a: Iterable[float], b: Iterable[float], fn: Callable[[float, float], float]
+):
+    """Combines elements from two iterables a and b using a given function
+
+    Args:
+        a (Iterable[float]): iterable a
+        b (Iterable[float]): iterable b
+        fn (Callable[[float, float], float]): a callable which combines elements from two iterables
+
+    Returns:
+        _type_: combination of two iterables after applying fn
+    """
+    assert len(a) == len(b)
+    return [fn(e1, e2) for (e1, e2) in zip(a, b)]
+
+
 # - reduce
+def reduce(reduce_fn: Callable[[float, float], float], start: float):
+    # Closure
+    def process(ls: Iterable[float]):
+        ans = start
+        for e in ls:
+            ans = reduce_fn(ans, e)
+        return ans
+
+    return process
+
+
 #
 # Use these to implement
 # - negList : negate a list
+def negList(ls: List[float]) -> List[float]:
+    """Negate all elements in a list
+
+    Args:
+        ls (List[float]): a list of floats
+
+    Returns:
+        _type_: negated list
+    """
+    return map(neg)(ls)
+
+
 # - addLists : add two lists together
+def addLists(ls1: List[float], ls2: List[float]) -> List[float]:
+    """Computes element-wise sum of two lists
+
+    Args:
+        ls1 (List[float]): list1
+        ls2 (List[float]): list2
+
+    Returns:
+        List[float]: element-wise sum of two lists
+    """
+    return zipWith(ls1, ls2, add)
+
+
 # - sum: sum lists
+def sum(ls: List[float]):
+    """Returns the sum of all elements in a list
+
+    Args:
+        ls (List[float]): a list
+
+    Returns:
+        _type_: sum of all elements in a list
+    """
+    return reduce(add, 0)(ls)
+
+
 # - prod: take the product of lists
+def prod(ls: List[float]):
+    """Returns the product of all elements in a list
 
+    Args:
+        ls (List[float]): a list
 
-# TODO: Implement for Task 0.3.
+    Returns:
+        _type_: product of all elements in a list
+    """
+    return reduce(mul, 1)(ls)
--- tests/test_operators.py
+++ tests/test_operators.py
@@ -105,10 +105,13 @@ def test_sigmoid(a: float) -> None:
     * It is always between 0.0 and 1.0.
     * one minus sigmoid is the same as sigmoid of the negative
     * It crosses 0 at 0.5
-    * It is  strictly increasing.
+    * It is strictly increasing.
     """
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert 0.0 <= sigmoid(a) <= 1.0
+    assert_close(1.0 - sigmoid(a), sigmoid(-a))
+    assert_close(sigmoid(0), 0.5)
+    assert lt(sigmoid(a - 100), sigmoid(a))
 
 
 @pytest.mark.task0_2
@@ -116,7 +119,8 @@ def test_sigmoid(a: float) -> None:
 def test_transitive(a: float, b: float, c: float) -> None:
     """Test the transitive property of less-than (a < b and b < c implies a < c)"""
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    if lt(a, b) and lt(b, c):
+        assert lt(a, c)
 
 
 @pytest.mark.task0_2
@@ -125,23 +129,28 @@ def test_symmetric() -> None:
     gives the same value regardless of the order of its input.
     """
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert mul(1, 2) == mul(2, 1)
 
 
 @pytest.mark.task0_2
-def test_distribute() -> None:
+@given(small_floats, small_floats, small_floats)
+def test_distribute(a: float, b: float, c: float) -> None:
     r"""Write a test that ensures that your operators distribute, i.e.
     :math:`z \times (x + y) = z \times x + z \times y`
     """
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert_close(mul(a, add(b, c)), add(mul(a, b), mul(a, c)))
 
 
 @pytest.mark.task0_2
-def test_other() -> None:
+@given(small_floats, small_floats)
+def test_other(a: float, b: float) -> None:
     """Write a test that ensures some other property holds for your functions."""
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    if a > 0:
+        assert relu_back(a, b) == b
+    else:
+        assert relu_back(a, b) == 0
 
 
 # ## Task 0.3  - Higher-order functions
@@ -169,7 +178,10 @@ def test_sum_distribute(ls1: List[float], ls2: List[float]) -> None:
     is the same as the sum of each element of `ls1` plus each element of `ls2`.
     """
     # TODO: Implement for Task 0.3.
-    raise NotImplementedError("Need to implement for Task 0.3")
+    loop_sum = [e1 + e2 for (e1, e2) in zip(ls1, ls2)]
+    zipWith_sum = addLists(ls1, ls2)
+    for i in range(len(loop_sum)):
+        assert_close(loop_sum[i], zipWith_sum[i])
 
 
 @pytest.mark.task0_3
@@ -188,6 +200,7 @@ def test_prod(x: float, y: float, z: float) -> None:
 @given(lists(small_floats))
 def test_negList(ls: List[float]) -> None:
     check = negList(ls)
+    # print(ls, check)
     for i, j in zip(ls, check):
         assert_close(i, -j)
 
